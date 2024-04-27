07ccfcbf8b785b144cfa57086a660237fe009f4d
Sergile Nyouvop <144280076+sn-goat@users.noreply.github.com>
2024-04-26 21:01:38 -0400

    Update maingui.cpp

--- maingui.cpp
+++ maingui.cpp
@@ -9,15 +9,23 @@ MainGui::MainGui(QWidget* widget): QGraphicsView(widget) {
     setFixedSize(1400,900);
 
     setScene(scene_);
+    activePiece = nullptr;
     QBrush brush;
     brush.setStyle(Qt::SolidPattern);
     brush.setColor(Qt::black);
     setBackgroundBrush(brush);
+
+    turnDisplay_ = new QGraphicsTextItem();
+    turnDisplay_->setPos(width()/2-100,10);
+    turnDisplay_->setZValue(1);
+    turnDisplay_->setDefaultTextColor(Qt::white);
+    turnDisplay_->setFont(QFont("",18));
+    turnDisplay_->setPlainText("Turn : WHITE");
 }
 
 void MainGui::drawBoard() {
     chessBoard_ = new Board();
-    chessBoard_->setBoard(width()/2-400,50, this);
+    chessBoard_->addSquareBoxes(width()/2-400,50, this);
 }
 
 
@@ -29,10 +37,31 @@ void MainGui::removeElementScene(QGraphicsItem* graphicsItem) {
     scene_->removeItem(graphicsItem);
 }
 
+ColorPiece  MainGui::getTurn(){
+    return turn_;
+}
+
+void MainGui::setTurn(ColorPiece turn){
+    turn_ = turn;
+}
+
+void MainGui::changeTurn(){
+    if(getTurn() == ColorPiece::WHITE){
+        setTurn(ColorPiece::BLACK);
+        turnDisplay_->setPlainText("Turn : BLACK");
+    }
+    else{
+        setTurn(ColorPiece::WHITE);
+        turnDisplay_->setPlainText("Turn : WHITE");
+    }
+}
+
 void MainGui::startGame(){
     chessBoard_->addPieces(this);
 }
 
 void MainGui::restartGame(){
+    activePieces.clear();
     chessBoard_->restartGame();
+    setTurn(ColorPiece::WHITE);
 }
