f6f0a26b841125dde1b8c25e0adece7f070f4ee9
Sergile Nyouvop <144280076+sn-goat@users.noreply.github.com>
2024-04-26 21:03:56 -0400

    Update raii_initialposition.cpp

--- raii_initialposition.cpp
+++ raii_initialposition.cpp
@@ -8,29 +8,17 @@ using namespace chess_game::logics;
 InitialPosition::InitialPosition(MainGui* mainGui, Board* board) {
     mainGui_ = mainGui;
     board_ = board;
-    for(int i = 0; i < board->ROW; ++i) {
-        for(int j = 0; j < board->COL; ++j){
+    for(int i = 0; i < board->NLINES; ++i) {
+        for(int j = 0; j < board->NCOLUMNS; ++j){
 
             SquareBox *squareBox = mainGui->allSquareBoxes[i][j];
             try
             {
-                if(i == 0 && j == 1) {
+                if(i == 0 && j == 5) {
                     board->addingBlackPiece(0, mainGui, squareBox);
                 }
-                if(i == 0 && j == 7) {
-                    board->addingBlackPiece(1, mainGui, squareBox);
-                }
-                if(i == 0 && j == 4) {
-                    board->addingBlackPiece(2, mainGui, squareBox);
-                }
-                if(i == 7 && j == 2) {
-                    board->addingWhitePiece(0, mainGui, squareBox);
-                }
-                if(i == 7 && j == 3) {
-                    board->addingWhitePiece(2, mainGui, squareBox);
-                }
                 if(i == 7 && j == 4) {
-                    board->addingWhitePiece(1, mainGui, squareBox);
+                    board->addingWhitePiece(0, mainGui, squareBox);
                 }
             }
             catch(PieceNonexistentException& e){
@@ -44,31 +32,19 @@ InitialPosition::InitialPosition(MainGui* mainGui, Board* board) {
 }
 
 InitialPosition::~InitialPosition(){
-    for(int i = 0; i < board_->ROW; ++i) {
-        for(int j = 0; j < board_->COL; ++j){
+    for(int i = 0; i < board_->NLINES; ++i) {
+        for(int j = 0; j < board_->NCOLUMNS; ++j){
 
             SquareBox* squareBox = mainGui_->allSquareBoxes[i][j];
             squareBox->resetBox();
 
             try
             {
-                if(i == 0 && j == 1) {
+                if(i == 0 && j == 5) {
                     board_->addingBlackPiece(0, mainGui_, squareBox);
                 }
-                if(i == 0 && j == 7) {
-                    board_->addingBlackPiece(1, mainGui_, squareBox);
-                }
-                if(i == 0 && j == 4) {
-                    board_->addingBlackPiece(2, mainGui_, squareBox);
-                }
-                if(i == 7 && j == 2) {
-                    board_->addingWhitePiece(0, mainGui_, squareBox);
-                }
-                if(i == 7 && j == 3) {
-                    board_->addingWhitePiece(2, mainGui_, squareBox);
-                }
                 if(i == 7 && j == 4) {
-                    board_->addingWhitePiece(1, mainGui_, squareBox);
+                    board_->addingWhitePiece(0, mainGui_, squareBox);
                 }
             }
             catch(PieceNonexistentException& e){
